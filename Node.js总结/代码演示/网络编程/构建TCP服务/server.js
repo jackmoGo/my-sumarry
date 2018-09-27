59ceff7bc7724c87b164af05690e3381e781df81
jackmo <jackmo@tencent.com>
2018-09-27 15:43:51 +0800

    tcp

--- Node.js总结/代码演示/网络编程/构建TCP服务/server.js
+++ Node.js总结/代码演示/网络编程/构建TCP服务/server.js
@@ -6,10 +6,17 @@ var server = net.createServer(function (socket){
     });
     socket.on('end', function (){
         console.log("连接断开");
-    })
+    });
+    socket.on('error', function(err){
+        console.log(err);
+    });
     socket.write("欢迎光临《深入浅出Node.js》示例：\n");
 })
 
 server.listen(8124, function (){
     console.log('server bound');
 });
+
+// server.listen('/tmp/echo1.sock', function (){
+//     console.log('server bound');
+// });
